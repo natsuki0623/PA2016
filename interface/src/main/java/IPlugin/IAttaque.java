e148060ec7a9968af0fb04201175b3adb8a605a2
Manon Ingrassia <manon.ingrassia@etu.unice.fr>
2016-11-16 11:08:10 +0100

    Création Interface Attaque

---
+++ interface/src/main/java/IPlugin/IAttaque.java
@@ -0,0 +1,11 @@
+package IPlugin;
+
+/*
+ * Interface permettant de gérer les plugins d'attaques
+ */
+
+public interface IAttaque {
+	public String toString();
+	public void attaqueDistance();
+	public void attaqueCaC();
+}
--- interface/src/main/java/IPlugin/classbidon.java
+++
@@ -1,9 +0,0 @@
-package IPlugin;
-
-/**
- * Class created on 16/11/2016
- *
- * @author JuIngong
- */
-public class classbidon {
-}
